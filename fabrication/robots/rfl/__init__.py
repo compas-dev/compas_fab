8af2a7bc1231aae818dbadb4693a7fd6a4700473
Gonzalo Casas <casas@arch.ethz.ch>
2017-03-02 14:41:10 +0100

    Add package init

---
+++ fabrication/robots/rfl/__init__.py
@@ -0,0 +1,18 @@
+"""
+.. _compas_fabrication.fabrication.robots.rfl:
+
+********************************************************************************
+compas_fabrication.fabrication.robots.rfl
+********************************************************************************
+
+.. module:: compas_fabrication.fabrication.robots.rfl
+
+Package with functionality to run simulations on the RFL (Robotic Fabrication
+Lab) using robotic simulation tools like
+`v-rep <http://www.coppeliarobotics.com/>`_.
+
+.. autosummary::
+    :toctree: generated/
+
+
+"""
