5a0692606929942170f5469935fc15638205274f
Gonzalo Casas <casas@arch.ethz.ch>
2017-08-10 17:53:54 +0200

    Add feature to generate paths to specific configurations instead of just goal poses/planes

--- fabrication/robots/rfl/simulator.py
+++ fabrication/robots/rfl/simulator.py
@@ -328,6 +328,108 @@ class Simulator(object):
 
         return self.add_building_member(robot, building_member_mesh)
 
+    def _find_path_plan(self, robot, goal, metric_values, collision_meshes,
+                        algorithm, trials, resolution,
+                        gantry_joint_limits, arm_joint_limits, shallow_state_search):
+        if not metric_values:
+            metric_values = [0.1] * robot.dof
+
+        if algorithm not in self.SUPPORTED_ALGORITHMS:
+            raise ValueError('Unsupported algorithm. Must be one of: ' + str(self.SUPPORTED_ALGORITHMS))
+
+        first_start = timer() if self.debug else None
+        if collision_meshes:
+            self.add_meshes(collision_meshes)
+        if self.debug:
+            LOG.debug('Execution time: add_meshes=%.2f', timer() - first_start)
+
+        start = timer() if self.debug else None
+        self.set_robot_metric(robot, metric_values)
+        if self.debug:
+            LOG.debug('Execution time: set_robot_metric=%.2f', timer() - start)
+
+        if 'target_type' not in goal:
+            raise SimulationError('Invalid goal type, you are using an internal function but passed incorrect args')
+
+        if goal['target_type'] == 'config':
+            states = []
+            for c in goal['target']:
+                states.extend(config_to_vrep(c, self.scale))
+        elif goal['target_type'] == 'pose':
+            start = timer() if self.debug else None
+            max_trials = None if shallow_state_search else 80
+            max_results = 1 if shallow_state_search else 80
+            states = self._find_raw_robot_states(robot, pose_to_vrep(goal['target'], self.scale), max_trials, max_results)
+            if self.debug:
+                LOG.debug('Execution time: search_robot_states=%.2f', timer() - start)
+
+        start = timer() if self.debug else None
+        string_param_list = [algorithm]
+        if gantry_joint_limits or arm_joint_limits:
+            joint_limits = []
+            joint_limits.extend(floats_to_vrep(gantry_joint_limits or [], self.scale))
+            joint_limits.extend(arm_joint_limits or [])
+            string_param_list.append(','.join(map(str, joint_limits)))
+
+        if self.debug:
+            LOG.debug('About to execute path planner: algorithm=%s, trials=%d, shallow_state_search=%s', algorithm, trials, shallow_state_search)
+
+        res, _, path, _, _ = self.run_child_script('searchRobotPath',
+                                                   [robot.index,
+                                                    trials,
+                                                    (int)(resolution * 1000)],
+                                                   states, string_param_list)
+        if self.debug:
+            LOG.debug('Execution time: search_robot_path=%.2f', timer() - start)
+
+        if res != 0:
+            raise SimulationError('Failed to search robot path', res)
+
+        if self.debug:
+            LOG.debug('Execution time: total=%.2f', timer() - first_start)
+
+        return [config_from_vrep(path[i:i + robot.dof], self.scale)
+                for i in range(0, len(path), robot.dof)]
+
+    def find_path_plan_to_config(self, robot, goal_configs, metric_values=None, collision_meshes=None,
+                                 algorithm='rrtconnect', trials=1, resolution=0.02,
+                                 gantry_joint_limits=None, arm_joint_limits=None, shallow_state_search=True):
+        """Finds a path plan to move the selected robot from its current position
+        to one of the `goal_configs`.
+
+        This function is useful when it is required to get a path plan that ends in one
+        specific goal configuration.
+
+        Args:
+            robot (:class:`.Robot`): Robot instance to move.
+            goal_configs (:obj:`list` of :class:`Configuration`): List of target or goal configurations.
+            metric_values (:obj:`list` of :obj:`float`): 9 :obj:`float`
+                values (3 for gantry + 6 for joints) ranging from 0 to 1,
+                where 1 indicates the axis/joint is blocked and cannot
+                move during inverse kinematic solving.
+            collision_meshes (:obj:`list` of :class:`compas.datastructures.mesh.Mesh`): Collision meshes
+                to be taken into account when calculating the motion plan.
+                Defaults to ``None``.
+            algorithm (:obj:`str`): Name of the algorithm to use. Defaults to ``rrtconnect``.
+            trials (:obj:`int`): Number of search trials to run. Defaults to ``1``.
+            resolution (:obj:`float`): Validity checking resolution. This value
+                is specified as a fraction of the space's extent.
+                Defaults to ``0.02``.
+            gantry_joint_limits (:obj:`list` of `float`): List of 6 floats defining the upper/lower limits of
+                gantry joints. Use this if you want to restrict the working area of the path planner.
+            arm_joint_limits (:obj:`list` of `float`): List of 12 floats defining the upper/lower limits of
+                arm joints. Use this if you want to restrict the working area of the path planner.
+            shallow_state_search (:obj:`bool`): True to search only a minimum of
+                valid states before searching a path, False to search states intensively.
+
+        Returns:
+            list: List of :class:`Configuration` objects representing the
+            collision-free path to the ``goal_pose``.
+        """
+        return self._find_path_plan(robot, {'target_type': 'config', 'target': goal_configs},
+                                    metric_values, collision_meshes, algorithm, trials, resolution,
+                                    gantry_joint_limits, arm_joint_limits, shallow_state_search)
+
     def find_path_plan(self, robot, goal_pose, metric_values=None, collision_meshes=None,
                        algorithm='rrtconnect', trials=1, resolution=0.02,
                        gantry_joint_limits=None, arm_joint_limits=None, shallow_state_search=True):
@@ -360,57 +462,9 @@ class Simulator(object):
             list: List of :class:`Configuration` objects representing the
             collision-free path to the ``goal_pose``.
         """
-        if not metric_values:
-            metric_values = [0.1] * robot.dof
-
-        if algorithm not in self.SUPPORTED_ALGORITHMS:
-            raise ValueError('Unsupported algorithm. Must be one of: ' + str(self.SUPPORTED_ALGORITHMS))
-
-        first_start = timer() if self.debug else None
-        if collision_meshes:
-            self.add_meshes(collision_meshes)
-        if self.debug:
-            LOG.debug('Execution time: add_meshes=%.2f', timer() - first_start)
-
-        start = timer() if self.debug else None
-        self.set_robot_metric(robot, metric_values)
-        if self.debug:
-            LOG.debug('Execution time: set_robot_metric=%.2f', timer() - start)
-
-        start = timer() if self.debug else None
-        max_trials = None if shallow_state_search else 80
-        max_results = 1 if shallow_state_search else 80
-        states = self._find_raw_robot_states(robot, pose_to_vrep(goal_pose, self.scale), max_trials, max_results)
-        if self.debug:
-            LOG.debug('Execution time: search_robot_states=%.2f', timer() - start)
-
-        start = timer() if self.debug else None
-        string_param_list = [algorithm]
-        if gantry_joint_limits or arm_joint_limits:
-            joint_limits = []
-            joint_limits.extend(floats_to_vrep(gantry_joint_limits or [], self.scale))
-            joint_limits.extend(arm_joint_limits or [])
-            string_param_list.append(','.join(map(str, joint_limits)))
-
-        if self.debug:
-            LOG.debug('About to execute path planner: algorithm=%s, trials=%d, shallow_state_search=%s', algorithm, trials, shallow_state_search)
-
-        res, _, path, _, _ = self.run_child_script('searchRobotPath',
-                                                   [robot.index,
-                                                    trials,
-                                                    (int)(resolution * 1000)],
-                                                   states, string_param_list)
-        if self.debug:
-            LOG.debug('Execution time: search_robot_path=%.2f', timer() - start)
-
-        if res != 0:
-            raise SimulationError('Failed to search robot path', res)
-
-        if self.debug:
-            LOG.debug('Execution time: total=%.2f', timer() - first_start)
-
-        return [config_from_vrep(path[i:i + robot.dof], self.scale)
-                for i in range(0, len(path), robot.dof)]
+        return self._find_path_plan(robot, {'target_type': 'pose', 'target': goal_pose},
+                                    metric_values, collision_meshes, algorithm, trials, resolution,
+                                    gantry_joint_limits, arm_joint_limits, shallow_state_search)
 
     def add_building_member(self, robot, building_member_mesh):
         """Adds a building member to the RFL scene and attaches it to the robot.
