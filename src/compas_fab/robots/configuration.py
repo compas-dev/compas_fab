c6bf2dbc58d42c08c7042a732e63e735bc3253de
Romana Rust <rust@arch.ethz.ch>
2018-08-27 14:39:05 +0200

    update configuration

--- src/compas_fab/robots/configuration.py
+++ src/compas_fab/robots/configuration.py
@@ -1,64 +1,78 @@
+import math
+
+from compas.robots import Joint
 
 class Configuration(object):
-    """Represents the configuration of a robot based on its
-    joint angle values and external axes values (if any).
+    """Represents the configuration of a robot based on its joints.
 
     Attributes:
-        joint_values (:obj:`list` of :obj:`float`): Joint values expressed
-            in degrees.
-        external_axes (:obj:`list` of :obj:`float`): Position on the external axis
-            system expressed in millimeters (if available).
+        values (:obj:`list` of :obj:`float`): Joint values expressed
+            in radians or m, depending on the respective type
+        types (:obj:`list` of :class:`Joint.TYPE`): Joint types, e.g. a list of 
+            Joint.REVOLUTE for revolute joints
 
     Examples:
 
+        >>> from math import pi
         >>> from compas_fab.robots import Configuration
-        >>> config = Configuration.from_data({'joint_values': [90., 0., 0.]})
-        >>> config.joint_values
-        [90.0, 0.0, 0.0]
+        >>> config = Configuration.from_revolute_values([pi/2, 0., 0.])
+        >>> config.values
+        [1.5707963267948966, 0.0, 0.0]
 
-
+        >>> from math import pi
         >>> from compas_fab.robots import Configuration
-        >>> config = Configuration.from_data({'joint_values': [90., 0., 0., 0., 180., 45.],\
-                                              'external_axes': [8312.0]})
+        >>> config = Configuration.from_prismatic_and_revolute_values([8.312], [pi/2, 0., 0., 0., 2*pi, 0.8])
         >>> str(config)
-        'joints: [90.0, 0.0, 0.0, 0.0, 180.0, 45.0], external_axes: [8312.0]'
+        Configuration(8.312, 1.571, 0.000, 0.000, 0.000, 6.283, 0.800)
+
+        >>> from math import pi
+        >>> from compas_fab.robots import Configuration
+        >>> from compas.robots import Joint
+        >>> config = Configuration([pi/2, 3., 0.1], [Joint.REVOLUTE, Joint.PRISMATIC, Joint.PLANAR])
+        Configuration(1.571, 3.000, 0.100)
 
     """
 
-    def __init__(self):
-        self.joint_values = None
-        self.external_axes = None
+    def __init__(self, values=[], types=[]):
+        if len(values) != len(types):
+            raise ValueError("%d values must have %d types, but %d given." % (len(values), len(values), len(types)))
+        self.values = values
+        self.types = types
+        self._precision = 3
 
     def __str__(self):
-        return "joints: %s, external_axes: %s" % (self.joint_values, self.external_axes)
+        vs = "%." + str(self._precision) + "f"
+        return "Configuration(%s)" % (", ".join([vs] * len(self.values)) % tuple(self.values))
 
     @classmethod
-    def from_joints(cls, joint_values):
-        """Construct a configuration from joint values.
+    def from_revolute_values(cls, values):
+        """Construct a configuration from revolute joint values in radians.
 
         Args:
-            joint_values (:obj:`list` of :obj:`float`): Joint values expressed
-                in degrees.
+            values (:obj:`list` of :obj:`float`): Joint values expressed in
+                radians.
 
         Returns:
             Configuration: A :class:`.Configuration` instance.
         """
-        return cls.from_joints_and_external_axes(joint_values, None)
+        return cls.from_data({'values': values, 'types': [Joint.REVOLUTE] * len(values)})
 
     @classmethod
-    def from_joints_and_external_axes(cls, joint_values, external_axes=None):
-        """Construct a configuration from joint values and external axes values.
+    def from_prismatic_and_revolute_values(cls, prismatic_values, revolute_values):
+        """Construct a configuration from prismatic and revolute joint values.
 
         Args:
-            joint_values (:obj:`list` of :obj:`float`): Joint values expressed
-                in degrees.
-            external_axes (:obj:`list` of :obj:`float`): Position on the external axis
-                system in millimeters (if available).
+            prismatic_values (:obj:`list` of :obj:`float`): Positions on the 
+                external axis system in meters.
+            revolute_values (:obj:`list` of :obj:`float`): Joint values 
+                expressed in radians.
 
         Returns:
             Configuration: A :class:`.Configuration` instance.
         """
-        return cls.from_data({'joint_values': joint_values, 'external_axes': external_axes})
+        values = prismatic_values + revolute_values
+        types = [Joint.PRISMATIC] * len(prismatic_values) + [Joint.REVOLUTE] * len(revolute_values)
+        return cls.from_data({'values': values, 'types': types})
 
     @classmethod
     def from_data(cls, data):
@@ -75,32 +89,40 @@ class Configuration(object):
         return config
 
     def to_data(self):
-        """Return the data dict that represents the configuration, and from which it can
-        be reconstructed."""
+        """Return the data dict that represents the configuration, and from 
+        which it can be reconstructed."""
         return self.data
 
     @property
     def data(self):
         """:obj:`dict` : The data representing the configuration.
 
-        By assigning a data dict to this property, the current data of the configuration
-        will be replaced by the data in the dict. The data getter and setter should
-        always be used in combination with each other.
+        By assigning a data dict to this property, the current data of the 
+        configuration will be replaced by the data in the dict. The data getter
+        and setter should always be used in combination with each other.
         """
         return {
-            'joint_values': self.joint_values,
-            'external_axes': self.external_axes
+            'values': self.values,
+            'types': self.types
         }
 
     @data.setter
     def data(self, data):
-        self.joint_values = data.get('joint_values') or None
-        self.external_axes = data.get('external_axes') or None
-
+        self.values = data.get('values') or []
+        self.types = data.get('types') or []
+    
 
 if __name__ == "__main__":
+    from math import pi
     q = [4.5, 1.7, 0.5, 2.1, 0.1, 2.1]
-    configuration = Configuration.from_joints(q)
+    configuration = Configuration.from_revolute_values(q)
     print(configuration)
-    from compas_fab.robots import Configuration
-    from compas_fab.robots import Pose
+
+    config = Configuration.from_revolute_values([pi/2, 0., 0.])
+    print(config.values)
+
+    config = Configuration.from_prismatic_and_revolute_values([8.312], [pi/2, 0., 0., 0., 2*pi, 0.8])
+    print(str(config))
+
+    config = Configuration([pi/2, 3., 0.1], [Joint.REVOLUTE, Joint.PRISMATIC, Joint.PLANAR])
+    print(str(config))
