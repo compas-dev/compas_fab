fcb89de6a57a393ef304cb5b69aaa7e2e6599e25
Gonzalo Casas <casas@arch.ethz.ch>
2018-09-05 07:13:45 +0200

    Add unit conversion functions

---
+++ src/compas_fab/robots/units.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+
+__all__ = [
+    'to_radians',
+    'to_degrees',
+]
+
+def to_radians(degrees):
+    """Converts a list of floats representing degrees to a list of radians.
+
+    Parameters
+    ----------
+    degrees : list of :obj:`float`
+        List of degrees.
+
+    Returns
+    -------
+    list of :obj:`float`
+        List of radians.
+    """
+    return map(math.radians, degrees)
+
+
+def to_degrees(radians):
+    """Converts a list of floats representing radians to a list of degrees.
+
+    Parameters
+    ----------
+    radians : list of :obj:`float`
+        List of radians.
+
+    Returns
+    -------
+    list of :obj:`float`
+        List of degress.
+    """
+    return map(math.degrees, radians)
