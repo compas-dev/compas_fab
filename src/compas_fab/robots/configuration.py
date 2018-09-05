b056b23a6900a09f04beab25a4f26ee2e1fadc33
Gonzalo Casas <casas@arch.ethz.ch>
2018-09-05 12:50:07 +0200

    Add properties for revolute and prismatic joint access, and update docstrings

--- src/compas_fab/robots/configuration.py
+++ src/compas_fab/robots/configuration.py
@@ -15,31 +15,33 @@ class Configuration(object):
     state of its joints. This concept is also refered to
     as `Joint State`.
 
-    Attributes:
-        values (:obj:`list` of :obj:`float`): Joint values expressed
-            in radians or meters, depending on the respective type
-        types (:obj:`list` of :class:`Joint.TYPE`): Joint types, e.g. a list of
-            Joint.REVOLUTE for revolute joints
+    Attributes
+    ----------
+    values : :obj:`list` of :obj:`float`
+        Joint values expressed in radians or meters, depending on the respective type.
+    types : :obj:`list` of :class:`Joint.TYPE`
+        Joint types, e.g. a list of ``Joint.REVOLUTE`` for revolute joints.
 
-    Examples:
+    Examples
+    --------
 
-        >>> from math import pi
-        >>> from compas_fab.robots import Configuration
-        >>> config = Configuration.from_revolute_values([pi/2, 0., 0.])
-        >>> config.values
-        [1.5707963267948966, 0.0, 0.0]
+    >>> from math import pi
+    >>> from compas_fab.robots import Configuration
+    >>> config = Configuration.from_revolute_values([pi/2, 0., 0.])
+    >>> config.values
+    [1.5707963267948966, 0.0, 0.0]
 
-        >>> from math import pi
-        >>> from compas_fab.robots import Configuration
-        >>> config = Configuration.from_prismatic_and_revolute_values([8.312], [pi/2, 0., 0., 0., 2*pi, 0.8])
-        >>> str(config)
-        Configuration(8.312, 1.571, 0.000, 0.000, 0.000, 6.283, 0.800)
+    >>> from math import pi
+    >>> from compas_fab.robots import Configuration
+    >>> config = Configuration.from_prismatic_and_revolute_values([8.312], [pi/2, 0., 0., 0., 2*pi, 0.8])
+    >>> str(config)
+    Configuration(8.312, 1.571, 0.000, 0.000, 0.000, 6.283, 0.800)
 
-        >>> from math import pi
-        >>> from compas_fab.robots import Configuration
-        >>> from compas.robots import Joint
-        >>> config = Configuration([pi/2, 3., 0.1], [Joint.REVOLUTE, Joint.PRISMATIC, Joint.PLANAR])
-        Configuration(1.571, 3.000, 0.100)
+    >>> from math import pi
+    >>> from compas_fab.robots import Configuration
+    >>> from compas.robots import Joint
+    >>> config = Configuration([pi/2, 3., 0.1], [Joint.REVOLUTE, Joint.PRISMATIC, Joint.PLANAR])
+    Configuration(1.571, 3.000, 0.100)
 
     """
 
@@ -58,12 +60,15 @@ class Configuration(object):
     def from_revolute_values(cls, values):
         """Construct a configuration from revolute joint values in radians.
 
-        Args:
-            values (:obj:`list` of :obj:`float`): Joint values expressed in
-                radians.
+        Parameters
+        ----------
+        values : :obj:`list` of :obj:`float`
+            Joint values expressed in radians.
 
-        Returns:
-            Configuration: A :class:`.Configuration` instance.
+        Returns
+        -------
+        :class:`Configuration`
+             An instance of :class:`Configuration` instance.
         """
         return cls.from_data({'values': values, 'types': [Joint.REVOLUTE] * len(values)})
 
@@ -71,14 +76,17 @@ class Configuration(object):
     def from_prismatic_and_revolute_values(cls, prismatic_values, revolute_values):
         """Construct a configuration from prismatic and revolute joint values.
 
-        Args:
-            prismatic_values (:obj:`list` of :obj:`float`): Positions on the
-                external axis system in meters.
-            revolute_values (:obj:`list` of :obj:`float`): Joint values
-                expressed in radians.
+        Parameters
+        ----------
+        prismatic_values : :obj:`list` of :obj:`float`
+            Positions on the external axis system in meters.
+        revolute_values : :obj:`list` of :obj:`float`
+            Joint values expressed in radians.
 
-        Returns:
-            Configuration: A :class:`.Configuration` instance.
+        Returns
+        -------
+        :class:`Configuration`
+             An instance of :class:`Configuration` instance.
         """
         values = prismatic_values + revolute_values
         types = [Joint.PRISMATIC] * len(prismatic_values) + [Joint.REVOLUTE] * len(revolute_values)
@@ -88,18 +96,22 @@ class Configuration(object):
     def from_data(cls, data):
         """Construct a configuration from its data representation.
 
-        Args:
-            data (`dict`): The data dictionary.
+        Parameters
+        ----------
+        data : :obj:`dict`
+            The data dictionary.
 
-        Returns:
-            Configuration: A :class:`Configuration` instance.
+        Returns
+        -------
+        :class:`Configuration`
+             An instance of :class:`Configuration` instance.
         """
         config = cls()
         config.data = data
         return config
 
     def to_data(self):
-        """Return the data dict that represents the configuration, and from
+        """Return the data dictionary that represents the configuration, and from
         which it can be reconstructed."""
         return self.data
 
@@ -107,7 +119,7 @@ class Configuration(object):
     def data(self):
         """:obj:`dict` : The data representing the configuration.
 
-        By assigning a data dict to this property, the current data of the
+        By assigning a data dictionary to this property, the current data of the
         configuration will be replaced by the data in the dict. The data getter
         and setter should always be used in combination with each other.
         """
@@ -121,9 +133,23 @@ class Configuration(object):
         self.values = data.get('values') or []
         self.types = data.get('types') or []
 
+    @property
+    def prismatic_values(self):
+        """:obj:`list` of :obj:`float` : Prismatic joint values in meters.
+
+        E.g. positions on the external axis system."""
+        return [v for i, v in enumerate(self.values) if self.types[i] == Joint.PRISMATIC]
+
+    @property
+    def revolute_values(self):
+        """:obj:`list` of :obj:`float` : Revolute joint values in radians."""
+        return [v for i, v in enumerate(self.values) if self.types[i] == Joint.REVOLUTE]
+
 
 if __name__ == "__main__":
     from math import pi
+    from math import radians
+
     q = [4.5, 1.7, 0.5, 2.1, 0.1, 2.1]
     configuration = Configuration.from_revolute_values(q)
     print(configuration)
@@ -136,3 +162,8 @@ if __name__ == "__main__":
 
     config = Configuration([pi/2, 3., 0.1], [Joint.REVOLUTE, Joint.PRISMATIC, Joint.PLANAR])
     print(str(config))
+
+    config = Configuration.from_prismatic_and_revolute_values([2., 3., 1.33], list(map(radians, [90, 0, 0, 20])))
+    print(str(config))
+    print(config.revolute_values)
+    print(config.prismatic_values)
