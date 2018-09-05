f7d64b5b37c4670d02b96d6d6fda553f730a0827
Gonzalo Casas <casas@arch.ethz.ch>
2018-09-05 13:23:59 +0200

    Docstrings

--- src/compas_fab/ghpython/artists.py
+++ src/compas_fab/ghpython/artists.py
@@ -1,10 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from compas_ghpython import mesh_draw
 from compas_ghpython.geometry import xform_from_transformation
 
 from compas_fab.robots.artists import BaseRobotArtist
 
+__all__ = [
+    'RobotArtist',
+]
+
 
 class RobotArtist(BaseRobotArtist):
+    """Visualizer for robots inside a Grasshopper environment."""
+
     def __init__(self, robot):
         super(RobotArtist, self).__init__(robot)
 
