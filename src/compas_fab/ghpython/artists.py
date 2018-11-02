26fd453a1c6d70faea1d48a4239b50fe961ca085
Romana Rust <rust@arch.ethz.ch>
2018-11-02 15:25:33 +0100

    Update src/compas_fab/ghpython/artists.py

--- src/compas_fab/ghpython/artists.py
+++ src/compas_fab/ghpython/artists.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from compas.utilities import rgb_to_rgb
+
 from compas_ghpython import mesh_draw
 from compas_ghpython.geometry import xform_from_transformation
 
@@ -23,6 +25,8 @@ class RobotArtist(BaseRobotArtist):
         native_mesh.Transform(T)
 
     def draw_mesh(self, compas_mesh, color=None):
+        if color:
+            color = rgb_to_rgb(color[0], color[1], color[2])
         return mesh_draw(compas_mesh, color=color)
 
     # def set_color(self, color_rgba):
