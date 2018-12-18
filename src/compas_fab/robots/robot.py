987a859a09cf9bb1a03a60c4dec90620b75c7a2d
Romana Rust <rust@arch.ethz.ch>
2018-12-18 19:30:43 +0100

    added motion_plan_configuration_goal and joint_positions_to_configuration

--- src/compas_fab/robots/robot.py
+++ src/compas_fab/robots/robot.py
@@ -189,6 +189,53 @@ class Robot(object):
         """
         configurable_joints = self.get_configurable_joints(group)
         return [j.name for j in configurable_joints]
+    
+    def get_configurable_joint_types(self, group=None):
+        """Returns all configurable joint types.
+
+        Parameters
+        ----------
+        group : str
+            The name of the group. Defaults to `None`.
+
+        Note
+        ----
+        If semantics is set and no group is passed, it returns all configurable
+        joint types of all groups.
+        """
+        configurable_joints = self.get_configurable_joints(group)
+        return [j.type for j in configurable_joints]
+    
+    def get_group_configuration(self, group, full_configuration):
+        """Returns the group's configuration.
+
+        Parameters
+        ----------
+        group : str
+            The name of the group. 
+        full_configuration (:class:`Configuration`): The configuration for all 
+            configurable joints of the robot.
+        """
+        values = []
+        types = []
+        group_joint_names = self.get_configurable_joint_names(group)
+        for i, name in enumerate(self.get_configurable_joint_names()):
+            if name in group_joint_names:
+                types.append(full_configuration.types[i])
+                values.append(full_configuration.values[i])
+        return Configuration(values, types)
+    
+    def joint_positions_to_configuration(self, joint_positions, group=None):
+        """Returns the robot's configuration from the passed joint_positions.
+        """
+        types = self.get_configurable_joint_types(group)
+        if len(types) != len(joint_positions) and group != None:
+            types = self.get_configurable_joint_types()
+            full_configuration = Configuration(joint_positions, types)
+            return self.get_group_configuration(group, full_configuration)
+        else:
+            return Configuration(joint_positions, types)
+
 
     def transformation_RCF_WCF(self, group=None):
         """Returns the transformation matrix from world coordinate system to 
@@ -380,7 +427,9 @@ class Robot(object):
                                            max_step_scaled, avoid_collisions)
     
     def motion_plan(self, frame_WCF, start_configuration, tolerance_position, 
-                    tolerance_angle, callback_result=None, group=None):
+                    tolerance_angle, callback_result=None, group=None, 
+                    path_constraints=None, trajectory_constraints=None,
+                    planner_id=None):
         """Calculates a motion from start_configuration to frame_WCF.
 
         Parameters
@@ -422,7 +471,61 @@ class Robot(object):
         
         self.client.motion_plan(callback_result, frame_RCF, base_link, ee_link,
                                 group, joint_names, joint_positions, 
-                                tolerance_position, tolerance_angle)
+                                tolerance_position, tolerance_angle, 
+                                path_constraints, trajectory_constraints,
+                                planner_id)
+    
+    def motion_plan_configuration_goal(self, goal_configuration, 
+                    start_configuration, tolerance, callback_result=None, 
+                    group=None, path_constraints=None, 
+                    trajectory_constraints=None, planner_id=None):
+        """Calculates a motion from start_configuration to end_configuration.
+
+        Parameters
+        ----------
+            goal_configuration (:class:`Frame`): The group's goal configuration.
+            start_configuration (:class:`Configuration`, optional): The robot's
+                configuration at the starting position.
+            tolerance (float or float list): the allowed tolerance to joints'
+                position. 
+            callback_result (function, optional): the function to call for the 
+                processing the result. Defaults to the print function.
+            group (str, optional): The planning group used for calculation.
+                Defaults to the robot's main planning group. 
+        
+        Examples
+        --------
+        """
+        self.ensure_client()
+        if not group:
+            group = self.main_group_name # ensure semantics
+        if not callback_result:
+            callback_result = print
+
+        base_link = self.get_base_link_name(group)
+        joint_names = self.get_configurable_joint_names()
+
+        joint_positions_goal = goal_configuration.values
+        joint_names_goal = self.get_configurable_joint_names(group)
+
+        if type(tolerance) != list:
+            tolerances = [tolerance] * len(joint_names_goal)
+        else:
+            if len(tolerance) != len(joint_names_goal):
+                raise ValueError("Please pass %d values for joint tolerances" % len(joint_names_goal))
+            tolerances = tolerance
+
+        if not start_configuration:
+            joint_positions = [0] * len(joint_names)
+        else:
+            if len(joint_names) != len(start_configuration.values):
+                raise ValueError("Please pass a configuration with %d values" % len(joint_names))
+            joint_positions = start_configuration.values
+        
+        self.client.motion_plan_joint_positions_goal(callback_result,         
+                    joint_positions_goal, joint_names_goal, tolerances, 
+                    base_link, group, joint_names, joint_positions, 
+                    path_constraints, trajectory_constraints, planner_id)
 
     def send_frame(self):
         # (check service name with ros)
