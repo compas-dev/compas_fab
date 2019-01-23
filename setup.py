41823d9db67ee4adf5a60c8636194dc72003008f
Gonzalo Casas <casas@arch.ethz.ch>
2019-01-23 14:26:10 +0100

    Pin latest compas and require minor of roslibpy

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ from os.path import abspath, basename, dirname, join, splitext
 from setuptools import find_packages, setup
 
 requirements = [
-    'compas==0.3.4',
-    'roslibpy',
+    'compas==0.4.6',
+    'roslibpy>=0.4.0',
     'pyserial',
 ]
 keywords_list = ['robotic fabrication', 'digital fabrication', 'architecture', 'robotics', 'ros']
