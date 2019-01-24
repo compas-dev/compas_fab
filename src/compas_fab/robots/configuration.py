f223cbdd03319dc48a9677d09eadb8e66215b97e
Romana Rust <rust@arch.ethz.ch>
2019-01-24 01:09:44 +0100

    update configuration __repr__

--- src/compas_fab/robots/configuration.py
+++ src/compas_fab/robots/configuration.py
@@ -55,7 +55,10 @@ class Configuration(object):
 
     def __str__(self):
         vs = '%.' + self._precision
-        return "Configuration(%s)" % (", ".join([vs] * len(self.values)) % tuple(self.values))
+        return "Configuration(%s, %s)" % ('(' + ", ".join([vs] * len(self.values)) % tuple(self.values) + ')', tuple(self.types))
+    
+    def __repr__(self):
+        return self.__str__()
 
     @classmethod
     def from_revolute_values(cls, values):
