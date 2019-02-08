37d30d15313ac590efc5cb53fb56c0ec03a4da80
Gonzalo Casas <casas@arch.ethz.ch>
2019-02-08 11:00:46 +0100

    Update installer to latest compas changes

--- src/compas_fab/rhino/install.py
+++ src/compas_fab/rhino/install.py
@@ -2,39 +2,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import sys
-
 import compas_rhino.install
 
 __all__ = []
 
-INSTALLABLE_PACKAGES = ('compas_fab', 'roslibpy')
-
-
-def install(version='6.0', packages=None):
-    """Install compas_fab for Rhino.
-
-    Parameters
-    ----------
-    version : {'5.0', '6.0'}
-        The version number of Rhino.
-    packages : list of str
-        List of packages to install or None to use default package list.
-
-    Examples
-    --------
-    .. code-block:: python
-
-        >>> import compas_fab
-        >>> compas_fab.rhino.install('6.0')
-
-    .. code-block:: python
-
-        $ python -m compas_fab.rhino.install 6.0
-
-    """
-
-    compas_rhino.install.install(version, packages)
+INSTALLABLE_PACKAGES = ['compas_fab', 'roslibpy']
 
 
 # ==============================================================================
@@ -43,20 +15,13 @@ def install(version='6.0', packages=None):
 
 if __name__ == "__main__":
 
-    import sys
+    import argparse
 
-    print('\nusage: python -m compas_fab.rhino.install [version]\n')
-    print('  version       Rhino version (5.0 or 6.0)\n')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', choices=['5.0', '6.0'], default='6.0', help="The version of Rhino to install the packages in.")
 
-    try:
-        version = sys.argv[1]
-    except IndexError:
-        version = '6.0'
-    else:
-        try:
-            version = str(version)
-        except Exception:
-            version = '6.0'
+    args = parser.parse_args()
 
     packages = set(compas_rhino.install.INSTALLABLE_PACKAGES + INSTALLABLE_PACKAGES)
-    install(version=version, packages=packages)
+
+    compas_rhino.install.install(version=args.version, packages=packages)
--- src/compas_fab/rhino/uninstall.py
+++ src/compas_fab/rhino/uninstall.py
@@ -1,43 +1,13 @@
-from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
-
-import os
-import sys
+from __future__ import print_function
 
 import compas_rhino
 import compas_rhino.uninstall
+
 import compas_fab.rhino.install
 
-from compas._os import remove_symlink
-
 __all__ = []
-
-
-def uninstall(version='6.0', packages=None):
-    """Uninstall compas_fab from Rhino.
-
-    Parameters
-    ----------
-    version : {'5.0', '6.0'}
-        The version number of Rhino.
-    packages : list of str
-        List of packages to uninstall or None to use default package list.
-
-    Examples
-    --------
-    .. code-block:: python
-
-        >>> import compas_fab
-        >>> compas_fab.rhino.uninstall('6.0')
-
-    .. code-block:: python
-
-        $ python -m compas_fab.rhino.uninstall 6.0
-
-    """
-
-    compas_rhino.uninstall.uninstall(version, packages)
 
 
 # ==============================================================================
@@ -46,21 +16,14 @@ def uninstall(version='6.0', packages=None):
 
 if __name__ == "__main__":
 
-    import sys
+    import argparse
 
-    print('\nusage: python -m compas_rhino.uninstall [version]\n')
-    print('  version       Rhino version (5.0 or 6.0)\n')
+    parser = argparse.ArgumentParser()
 
-    try:
-        version = sys.argv[1]
-    except IndexError:
-        version = '6.0'
-    else:
-        try:
-            version = str(version)
-        except Exception:
-            version = '6.0'
+    parser.add_argument('-v', '--version', choices=['5.0', '6.0'], default='5.0', help="The version of Rhino to install the packages in.")
 
-    packages = set(compas_rhino.install.INSTALLABLE_PACKAGES +
-                   compas_fab.rhino.install.INSTALLABLE_PACKAGES)
-    uninstall(version=version, packages=packages)
+    args = parser.parse_args()
+
+    packages = set(compas_rhino.install.INSTALLABLE_PACKAGES + compas_fab.rhino.install.INSTALLABLE_PACKAGES)
+
+    compas_rhino.uninstall.uninstall(version=args.version, packages=packages)
