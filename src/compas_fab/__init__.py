edf3af0b2102f797ad1497e2dfb51f7efaa4232c
Romana Rust <rust@arch.ethz.ch>
2019-03-11 13:44:32 +0100

    fixing data path and added compas_fab.get(filename)

--- src/compas_fab/__init__.py
+++ src/compas_fab/__init__.py
@@ -29,13 +29,15 @@ import os
 from .__version__ import __author__, __author_email__, __copyright__, __description__, __license__, __title__, __url__, __version__
 
 HERE = os.path.dirname(__file__)
-DATA = os.path.abspath(os.path.join(HERE, 'data'))
+DATA = os.path.abspath(os.path.join(HERE, "..", "..", 'data'))
 
 
 def _find_resource(filename):
     filename = filename.strip('/')
     return os.path.abspath(os.path.join(DATA, filename))
 
+def get(filename):
+    return _find_resource(filename)
 
 def get_data(filename):
     return _find_resource(filename)
