6ef5d12bf97beacb4407fc94bb7bda51fc8f32e6
Romana Rust <rust@arch.ethz.ch>
2019-03-27 18:14:00 +0100

    added ur5 robot as example robot

---
+++ src/compas_fab/robots/ur5.py
@@ -0,0 +1,69 @@
+"""This module is mostly for example purposes.
+
+It needs to be explicitely imported, since it is not a fundamental part
+of the compas_fab.robots package at all.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from compas.robots import RobotModel
+from compas_fab.robots import RobotSemantics
+from compas.robots import LocalPackageMeshLoader
+import compas_fab
+
+
+__all__ = [
+    'Robot',
+]
+
+def Robot(client=None, load_geometry=False):
+    """Returns a UR5 robot.
+
+    This method serves mainly as help for writing examples, so that the code can
+    stay short.
+
+    It is intentionally capitalized to act as an almost drop-in replacement for
+    the constructor of :class:`compas_fab.robots.Robot`.
+
+    Parameters
+    ----------
+    client: object
+        Backend client.
+    load_geometry: bool
+
+    Returns
+    -------
+    :class:`compas_fab.robots.Robot`
+        Newly created instance of a UR5 robot.
+
+    Examples
+    --------
+
+    >>> from compas_fab.robots.ur5 import Robot
+    >>> robot = Robot()
+    >>> robot.name
+    'ur5'
+    """
+    
+    urdf_filename = compas_fab.get("universal_robot/ur_description/urdf/ur5.urdf")
+    srdf_filename = compas_fab.get("universal_robot/ur5_moveit_config/config/ur5.srdf")
+
+    model = RobotModel.from_urdf_file(urdf_filename)
+    semantics = RobotSemantics.from_srdf_file(srdf_filename, model)
+
+    if load_geometry:
+        loader = LocalPackageMeshLoader(compas_fab.get("universal_robot"), "ur_description")
+        model.load_geometry(loader)
+
+    robot = compas_fab.robots.robot.Robot(model, semantics=semantics)
+
+    if client:
+        robot.client = client
+
+    return robot
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
