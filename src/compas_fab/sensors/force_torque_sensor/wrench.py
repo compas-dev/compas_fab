9bfc9c026d82cc28ba987fb04f877e9bfa97a641
matteo-pacher <32835868+matteo-pacher@users.noreply.github.com>
2019-05-28 15:00:16 +0200

    Force Torque sensor
    - interafaces to receive data from ROS node through RosLibPy
    - compensation methods from gravity compensation and unbiasing
    - mass and center of mass estimation
    - set bias as ROS parameter
    - Wrench class WIP
    
    (cherry picked from commit 34e0e98299a9c6c8bcb6ce63fbbe7b216e25bf0a)

---
+++ src/compas_fab/sensors/force_torque_sensor/wrench.py
@@ -0,0 +1,17 @@
+
+# TO BE IMPLEMENTED PROPERLY YET
+
+class Wrench():
+
+	"""
+	A wrench is defined by...
+
+	Parameters
+	----------
+	wrench : list
+		List of 6 axis forces and torque
+	"""
+
+	def __init__(self, wrench):
+		self.wrench = wrench
+			
