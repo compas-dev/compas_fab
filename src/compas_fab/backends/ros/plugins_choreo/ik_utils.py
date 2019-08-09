299d6d853abf2ab39e4d94602cc3d96cfab5d89f
Yijiang Huang <yijiangh@mit.edu>
2019-08-10 01:57:56 +0200

    IKfast modules w/ pybullet added and tested

--- src/compas_fab/backends/ros/plugins_choreo/ik_utils.py
+++ src/compas_fab/backends/ros/plugins_choreo/ik_utils.py
@@ -14,15 +14,12 @@ from __future__ import print_function
 
 from conrob_pybullet import multiply, get_link_pose, get_movable_joints, \
     link_from_name, get_joint_positions, invert, violates_limits, get_pose, \
-    get_distance, joints_from_names, quat_from_matrix
-
-# TODO: default options, should be removed later
-USE_ALL = False
-USE_CURRENT = None
+    get_distance, joints_from_names, quat_from_matrix, matrix_from_quat, \
+    point_from_pose, quat_from_pose
 
 
 def get_ik_tool_link_pose(fk_fn, robot, ik_joint_names, base_link_name, \
-                          joint_values=None, use_current=USE_CURRENT):
+                          joint_values=None, use_current=False):
     """Use the given forward_kinematics function to compute ik_tool_link pose
     based on current joint configurations in pybullet.
 
@@ -35,10 +32,15 @@ def get_ik_tool_link_pose(fk_fn, robot, ik_joint_names, base_link_name, \
     robot : pybullet robot
     ik_joint_names : list of str
         a list of joint names that is registered for IK/FK
+        e.g. ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
+              'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
     base_link_name : str
-        robot base link's name, usually it's simply 'base_link'
+        robot base link's name, usually it's 'base_link'
     joint_values : list of float
         robot joint values for FK computation, value correponds to ik_joint_names
+    use_current: bool
+        if true, use the current configuration in pybullet env for FK, default
+        to False
 
     Returns
     -------
@@ -58,61 +60,95 @@ def get_ik_tool_link_pose(fk_fn, robot, ik_joint_names, base_link_name, \
     return multiply(world_from_base, base_from_tool)
 
 
-# def get_tool_from_ik(robot):
-#     world_from_tool = get_link_pose(robot, link_from_name(robot, TOOL_FRAME))
-#     world_from_ik = get_link_pose(robot, link_from_name(robot, IK_FRAME))
-#     # tool from the bare flange (6th axis)
-#     return multiply(invert(world_from_tool), world_from_ik)
+def get_ik_generator(ik_fn, robot, base_link_name, world_from_tcp, ik_tool_link_from_tcp=None):
+    """get an ik generator
 
+    Parameters
+    ----------
+    ik_fn : function handle
+        get_ik(point, rot) : list of jt solutions
+        point = [x,y,z]
+        rot = 3x3 rotational matrix as a row-major list
+    robot : pybullet robot
+    base_link_name : str
+        robot base link's name, usually it's 'base_link'
+    world_from_tcp : pybullet pose
+        tcp pose in the world frame
+    ik_tool_link_from_tcp : pybullet pose
+        tcp pose in ik tool link's frame, optional, defaults = None
 
-def get_ik_generator(ik_fn, robot, base_link_name, world_from_tcp, ik_tool_link_from_tcp):
+    Returns
+    -------
+    generator
+        use next() to get solutions
+
+    """
     world_from_base = get_link_pose(robot, link_from_name(robot, base_link_name))
     base_from_tcp = multiply(invert(world_from_base), world_from_tcp)
-    base_from_ik_tool_link = multiply(base_from_tcp, invert(ik_tool_link_from_tcp))
+    if ik_tool_link_from_tcp:
+        base_from_ik_tool_link = multiply(base_from_tcp, invert(ik_tool_link_from_tcp))
+    else:
+        base_from_ik_tool_link = base_from_tcp
     yield compute_inverse_kinematics(ik_fn, base_from_ik_tool_link)
 
 
-def sample_tool_ik(ik_fn, robot, ik_joints, base_link_name, world_from_tcp, ik_tool_link_from_tcp, closest_only=False, get_all=False, **kwargs):
+def sample_tool_ik(ik_fn, robot, ik_joint_names, base_link_name, world_from_tcp, ik_tool_link_from_tcp=None, get_all=False, **kwargs):
     """ sample ik joints for a given tcp pose in the world frame
 
     Parameters
     ----------
-    ik_fn : type
-        Description of parameter `ik_fn`.
-    robot : type
-        Description of parameter `robot`.
-    ik_joints : type
-        Description of parameter `ik_joints`.
-    base_link_name : type
-        Description of parameter `base_link_name`.
-    world_from_tcp : type
-        Description of parameter `world_from_tcp`.
-    ik_tool_link_from_tcp : type
-        Description of parameter `ik_tool_link_from_tcp`.
-    closest_only : type
-        Description of parameter `closest_only`.
-    get_all : type
-        Description of parameter `get_all`.
-    **kwargs : type
-        Description of parameter `**kwargs`.
+    ik_fn : function handle
+        get_ik(point, rot) : list of jt solutions
+        point = [x,y,z]
+        rot = 3x3 rotational matrix as a row-major list
+    robot : pybullet robot
+    ik_joint_names : list of str
+        a list of joint names that is registered for IK/FK
+        e.g. ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
+              'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+    base_link_name : str
+        robot base link's name, usually it's 'base_link'
+    world_from_tcp : pybullet pose
+        tcp pose in the world frame
+    ik_tool_link_from_tcp : pybullet pose
+        tcp pose in ik tool link's frame, optional, defaults to None
+    get_all : bool
+        get all ik sol computed
+    **kwargs : optional arguments
+        for solution selection, nearby_conf=True to select the one that's closest
+        to current conf in pybullet env
 
     Returns
     -------
-    type
-        Description of returned object.
+    a list of 6-list
+        computed IK solutions that satisfy joint limits
 
     """
+    ik_joints = joints_from_names(robot, ik_joint_names)
     generator = get_ik_generator(ik_fn, robot, base_link_name, world_from_tcp, ik_tool_link_from_tcp)
-    # ik_joints = get_movable_joints(robot)
     solutions = next(generator)
-    if closest_only and solutions:
-        current_conf = get_joint_positions(robot, ik_joints)
-        solutions = [min(solutions, key=lambda conf: get_distance(current_conf, conf))]
     solutions = list(filter(lambda conf: not violates_limits(robot, ik_joints, conf), solutions))
     return solutions if get_all else select_solution(robot, ik_joints, solutions, **kwargs)
 
 
 def compute_forward_kinematics(fk_fn, conf):
+    """use the given fk function to compute FK
+
+    Parameters
+    ----------
+    fk_fn : function handle
+    conf : list
+        joint values
+
+    Returns
+    -------
+    pos: 3-list
+        [x,y,z] of the FK solution pose
+
+    quat: 4-list
+        quaternion of the FK solution pose
+
+    """
     pose = fk_fn(list(conf))
     pos, rot = pose
     quat = quat_from_matrix(rot) # [X,Y,Z,W]
@@ -120,33 +156,69 @@ def compute_forward_kinematics(fk_fn, conf):
 
 
 def compute_inverse_kinematics(ik_fn, pose, sampled=[]):
+    """compute ik solutions using the given ik function handle
+
+    Parameters
+    ----------
+    ik_fn : function handle
+        get_ik(point, rot) : list of jt solutions
+        point = [x,y,z]
+        rot = 3x3 rotational matrix as a row-major list
+    pose : pybullet pose
+        pose of the ik tool link
+    sampled : list
+        a list of externally sampled solutions that wants to be appended to the
+        computed ik solutions
+
+    Returns
+    -------
+    a list of 6-lists
+        a list of ik solutions
+
+    """
     pos = point_from_pose(pose)
     rot = matrix_from_quat(quat_from_pose(pose)).tolist()
     if sampled:
-        solutions = ik_fn(list(rot), list(pos), sampled)
+        solutions = ik_fn(list(pos), list(rot), sampled)
     else:
-        solutions = ik_fn(list(rot), list(pos))
+        solutions = ik_fn(list(pos), list(rot))
     if solutions is None:
         return []
     return solutions
 
 
-def get_ik_limits(robot, joint, limits=USE_ALL):
-    if limits is USE_ALL:
-        return get_joint_limits(robot, joint)
-    elif limits is USE_CURRENT:
-        value = get_joint_position(robot, joint)
-        return value, value
-    return limits
+def select_solution(body, joints, solutions, nearby_conf=True, random=False, **kwargs):
+    """select one joint configuration given a list of them
 
+    Parameters
+    ----------
+    body : pybullet body
+        This can be any pybullet body, including the robot
+    joints : a list of pybullet joint
+    solutions : a list of float-lists
+        joint values
+    nearby_conf : bool
+        return the joint conf that is closest to the current conf in pybullet env,
+        defaults to True
+    random : bool
+        randomly chose one
+    **kwargs : optional
+        additional arguments for the cost function when ranking
 
-def select_solution(body, joints, solutions, nearby_conf=USE_ALL, **kwargs):
+    Returns
+    -------
+    a list of lists or one list
+        the joint configuration(s)
+
+    """
     if not solutions:
         return None
-    if nearby_conf is USE_ALL:
+    if random and not nearby_conf:
         return random.choice(solutions)
-    if nearby_conf is USE_CURRENT:
+    if nearby_conf:
         nearby_conf = get_joint_positions(body, joints)
-    # TODO: sort by distance before collision checking
-    # TODO: search over neighborhood of sampled joints when nearby_conf != None
-    return min(solutions, key=lambda conf: get_distance(nearby_conf, conf, **kwargs))
+        # TODO: sort by distance before collision checking
+        # TODO: search over neighborhood of sampled joints when nearby_conf != None
+        return min(solutions, key=lambda conf: get_distance(nearby_conf, conf, **kwargs))
+    else:
+        return random.choice(solutions)
--- tests/test_ikfast.py
+++ tests/test_ikfast.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import time
 import pytest
+import numpy as np
 from numpy.testing import assert_array_almost_equal
 
 from compas.geometry import Frame
@@ -21,13 +22,14 @@ from compas_fab.robots.ur5 import Robot
 from compas_fab.backends.pybullet import attach_end_effector_geometry, \
 convert_mesh_to_pybullet_body, get_TCP_pose, create_pb_robot_from_ros_urdf
 
-from compas_fab.backends.ros.plugins_choreo import get_ik_tool_link_pose
+from compas_fab.backends.ros.plugins_choreo import get_ik_tool_link_pose, \
+ sample_tool_ik
 
 from conrob_pybullet import load_pybullet, connect, disconnect, wait_for_user, \
     LockRenderer, has_gui, get_model_info, get_pose, euler_from_quat, draw_pose, \
     get_link_pose, link_from_name, create_attachment, add_fixed_constraint, \
     create_obj, set_pose, get_sample_fn, violates_limits, joints_from_names, \
-    set_joint_positions
+    set_joint_positions, remove_debug, get_joint_limits
 
 try:
     import ikfast_ur5
@@ -35,7 +37,6 @@ except ImportError as e:
     assert False, '\x1b[6;30;43m' + '{}, please install ikfast_pybind'.format(e) + '\x1b[0m'
 
 
-@pytest.mark.wip
 def test_ikfast_forward_kinematics():
     """TODO: this test_function can by pybullet-free"""
     urdf_filename = compas_fab.get('universal_robot/ur_description/urdf/ur5.urdf')
@@ -76,7 +77,97 @@ def test_ikfast_forward_kinematics():
         assert_array_almost_equal(TCP_pb_pose[0], ikfast_FK_pb_pose[0])
         assert_array_almost_equal(TCP_pb_pose[1], ikfast_FK_pb_pose[1])
 
-    # if has_gui():
-    #     draw_pose(ikfast_FK_pb_pose, length=0.04)
-    #     draw_pose(TCP_pb_pose, length=0.04)
-    #     wait_for_user()
+
+def best_sol(sols, q_guess, weights):
+    """get the best solution based on UR's joint domain value and weighted joint diff
+
+    ported from https://github.com/ros-industrial/universal_robot/blob/kinetic-devel/ur_kinematics/src/ur_kinematics/test_analytical_ik.py
+
+    Specific for UR setup (4 pi domain ranges)
+    """
+    valid_sols = []
+    for sol in sols:
+        test_sol = np.ones(6)*9999.
+        for i in range(6):
+            for add_ang in [-2.*np.pi, 0, 2.*np.pi]:
+                test_ang = sol[i] + add_ang
+                if (abs(test_ang) <= 2.*np.pi and
+                    # snap solution to the one that's closer to q_guess
+                    abs(test_ang - q_guess[i]) < abs(test_sol[i] - q_guess[i])):
+                    test_sol[i] = test_ang
+        if np.all(test_sol != 9999.):
+            valid_sols.append(test_sol)
+    if len(valid_sols) == 0:
+        return None
+    best_sol_ind = np.argmin(np.sum((weights*(valid_sols - np.array(q_guess)))**2,1))
+    return valid_sols[best_sol_ind]
+
+
+@pytest.mark.wip
+def test_ikfast_forward_kinematics():
+    """TODO: this test_function can by pybullet-free"""
+    urdf_filename = compas_fab.get('universal_robot/ur_description/urdf/ur5.urdf')
+    srdf_filename = compas_fab.get('universal_robot/ur5_moveit_config/config/ur5.srdf')
+    urdf_pkg_name = 'ur_description'
+
+    model = RobotModel.from_urdf_file(urdf_filename)
+    semantics = RobotSemantics.from_srdf_file(srdf_filename, model)
+    robot = RobotClass(model, semantics=semantics)
+
+    base_link_name = robot.get_base_link_name()
+    ik_joint_names = robot.get_configurable_joint_names()
+    ik_tool_link_name = robot.get_end_effector_link_name()
+
+    connect(use_gui=False)
+    pb_robot = create_pb_robot_from_ros_urdf(urdf_filename, urdf_pkg_name)
+    pb_ik_joints = joints_from_names(pb_robot, ik_joint_names)
+
+    max_attempts = 20
+    EPS = 1e-3
+    sample_fn = get_sample_fn(pb_robot, pb_ik_joints)
+
+    for i in range(max_attempts):
+        # randomly sample within joint limits
+        conf = sample_fn()
+        # sanity joint limit violation check
+        assert not violates_limits(pb_robot, pb_ik_joints, conf)
+
+        # ikfast's FK
+        fk_fn = ikfast_ur5.get_fk
+        ikfast_FK_pb_pose = get_ik_tool_link_pose(fk_fn, pb_robot, ik_joint_names, base_link_name, conf)
+
+        if has_gui():
+            print('test round #{}: ground truth conf: {}'.format(i, conf))
+            handles = draw_pose(ikfast_FK_pb_pose, length=0.04)
+            set_joint_positions(pb_robot, pb_ik_joints, conf)
+            wait_for_user()
+
+        # ikfast's IK
+        ik_fn = ikfast_ur5.get_ik
+        ik_sols = sample_tool_ik(ik_fn, pb_robot, ik_joint_names, base_link_name,
+                        ikfast_FK_pb_pose, get_all=True)
+
+        # TODO: UR robot or in general joint w/ domain over 4 pi
+        # needs specialized distance function
+        q_selected = sample_tool_ik(ik_fn, pb_robot, ik_joint_names, base_link_name,
+                        ikfast_FK_pb_pose, nearby_conf=True)
+        qsol = best_sol(ik_sols, conf, [1.]*6)
+        # print('q selected: {}'.format(q_selected))
+        # print('q best: {}'.format(qsol))
+
+        if has_gui():
+            set_joint_positions(pb_robot, pb_ik_joints, qsol)
+            wait_for_user()
+            for h in handles:
+                remove_debug(h)
+
+        if qsol is None:
+            qsol = [999.]*6
+        diff = np.sum(np.abs(np.array(qsol) - np.array(conf)))
+        if diff > EPS:
+            print(np.array(ik_sols))
+            print('Best q:{}'.format(qsol))
+            print('Actual:{}'.format(np.array(conf)))
+            print('Diff:  {}'.format(conf - qsol))
+            print('Difdiv:{}'.format((conf - qsol)/np.pi))
+            assert False
