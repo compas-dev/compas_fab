af9821288dfbd06b800b8fe2402417be64edae12
Romana Rust <rust@arch.ethz.ch>
2019-10-28 06:17:00 +0100

    Update __init__.py

--- src/compas_fab/backends/ros/messages/__init__.py
+++ src/compas_fab/backends/ros/messages/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-from .std_msgs import *
 from .actionlib_msgs import *
 from .control_msgs import *
 from .geometry_msgs import *
@@ -9,6 +8,7 @@ from .object_recognition_msgs import *
 from .octomap_msgs import *
 from .sensor_msgs import *
 from .shape_msgs import *
+from .std_msgs import *
 from .trajectory_msgs import *
 
 from .services import *
