1159dc22f78ef93c93abaaa76d9e7f71f1566957
Gonzalo Casas <casas@arch.ethz.ch>
2019-11-07 00:27:24 +0100

    Add linter exceptions to all __init__ imports

--- src/compas_fab/backends/ros/messages/__init__.py
+++ src/compas_fab/backends/ros/messages/__init__.py
@@ -1,14 +1,13 @@
 from __future__ import absolute_import
 
-from .actionlib_msgs import *
-from .control_msgs import *
-from .geometry_msgs import *
-from .moveit_msgs import *
-from .object_recognition_msgs import *
-from .octomap_msgs import *
-from .sensor_msgs import *
-from .shape_msgs import *
-from .std_msgs import *
-from .trajectory_msgs import *
-
-from .services import *
+from .actionlib_msgs import *           # noqa: F401,F403
+from .control_msgs import *             # noqa: F401,F403
+from .geometry_msgs import *            # noqa: F401,F403
+from .moveit_msgs import *              # noqa: F401,F403
+from .object_recognition_msgs import *  # noqa: F401,F403
+from .octomap_msgs import *             # noqa: F401,F403
+from .sensor_msgs import *              # noqa: F401,F403
+from .services import *                 # noqa: F401,F403
+from .shape_msgs import *               # noqa: F401,F403
+from .std_msgs import *                 # noqa: F401,F403
+from .trajectory_msgs import *          # noqa: F401,F403
