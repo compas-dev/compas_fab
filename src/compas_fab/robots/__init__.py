74aa6e6d2485b23084bab5faf6fbc6e91230c7ed
Gonzalo Casas <casas@arch.ethz.ch>
2019-11-19 17:13:53 +0100

    Fix conftest

--- src/compas_fab/robots/__init__.py
+++ src/compas_fab/robots/__init__.py
@@ -79,9 +79,9 @@ from .units import *                  # noqa: F401,F403
 from .robot import *                  # noqa: F401,F403
 from .semantics import *              # noqa: F401,F403
 from .time_ import *                  # noqa: F401,F403
+from .tool import *                   # noqa: F401,F403
 from .trajectory import *             # noqa: F401,F403
 from .wrench import *                 # noqa: F401,F403
 from .inertia import *                # noqa: F401,F403
-from .tool import *                   # noqa: F401,F403
 
 __all__ = [name for name in dir() if not name.startswith('_')]
--- src/compas_fab/robots/conftest.py
+++ src/compas_fab/robots/conftest.py
@@ -1,17 +1,17 @@
 import math
-import time
 import os
+import time
 
 import pytest
 from compas.datastructures import Mesh
 from compas.geometry import Frame
-from compas.geometry import Scale
 from compas.geometry import Rotation
+from compas.geometry import Scale
 
 import compas_fab
 from compas_fab.backends import RosClient
-from compas_fab.robots.ur5 import Robot
 from compas_fab.robots.tool import Tool
+from compas_fab.robots.ur5 import Robot
 
 
 @pytest.fixture(autouse=True)
@@ -24,7 +24,7 @@ def add_imports(doctest_namespace):
     doctest_namespace["Scale"] = Scale
     doctest_namespace["compas_fab"] = compas_fab
     doctest_namespace["Rotation"] = Rotation
-    doctest_namespace["Rotation"] = Tool
+    doctest_namespace["Tool"] = Tool
 
 
 @pytest.fixture(scope="session", autouse=True)
