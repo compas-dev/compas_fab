1d8f07f7bdcd05d7b8116ae94573d1eb9b4dbd46
Romana Rust <rust@arch.ethz.ch>
2020-04-06 08:36:42 +0200

    removin tangent_points_to_circle, now in compas

--- src/compas_fab/backends/ik_analytical/ik_spherical_wrist.py
+++ src/compas_fab/backends/ik_analytical/ik_spherical_wrist.py
@@ -6,38 +6,7 @@ from compas.geometry import Rotation
 from compas.geometry import Sphere
 from compas.geometry import intersection_sphere_sphere
 from compas.geometry import intersection_plane_circle
-
-
-def tangent_points_to_circle_xy(circle, point):
-    """Calculates the tangent points on a circle in the XY plane.
-
-    Parameters
-    ----------
-    circle : tuple
-        center, radius of the circle in the xy plane.
-    point : tuple
-        XY(Z) coordinates of a point in the xy plane.
-
-    Returns
-    -------
-    points : list of tuples
-        the tangent points on the circle
-    """
-    m, r = circle[0], circle[1]
-    cx, cy = m[0], m[1]
-    px = point[0] - cx
-    py = point[1] - cy
-
-    a1 = r*(px*r - py*math.sqrt(px**2 + py**2 - r**2))/(px**2 + py**2)
-    a2 = r*(px*r + py*math.sqrt(px**2 + py**2 - r**2))/(px**2 + py**2)
-
-    b1 = (r**2 - px*a1)/py
-    b2 = (r**2 - px*a2)/py
-
-    p1 = (a1 + cx, b1 + cy, 0)
-    p2 = (a2 + cx, b2 + cy, 0)
-    return p1, p2
-
+from compas.geometry import tangent_points_to_circle_xy
 
 
 def forward_kinematics_spherical_wrist(p1, p2, p3, p4, joint_values):
@@ -226,7 +195,7 @@ def inverse_kinematics_spherical_wrist(p1, p2, p3, p4, target_frame):
                 axis6_angle = math.atan2(endy, endx)
                 axis6_angles.append(axis6_angle)
     
-    for joint_values in zip(axis1_angles, axis2_angles, axis3_angles, axis4_angles, axis5_angles, axis6_angles):
-        print(forward_kinematics_spherical_wrist(p1, p2, p3, p4, joint_values))
+    #for joint_values in zip(axis1_angles, axis2_angles, axis3_angles, axis4_angles, axis5_angles, axis6_angles):
+    #    print(forward_kinematics_spherical_wrist(p1, p2, p3, p4, joint_values))
 
     return axis1_angles, axis2_angles, axis3_angles, axis4_angles, axis5_angles, axis6_angles
