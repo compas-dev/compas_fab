f306210a418afd60f0467f062e4b262e44faad59
Romana Rust <rust@arch.ethz.ch>
2020-09-08 10:58:34 +0200

    adding differences to configuration

--- src/compas_fab/robots/configuration.py
+++ src/compas_fab/robots/configuration.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from math import pi
 from compas.robots import Joint
 
 __all__ = [
@@ -247,9 +248,110 @@ class Configuration(object):
         config.scale(scale_factor)
         return config
 
+    def differences_generator(self, other):
+        """Generator over the differences to another `Configuration`'s values.
+
+        If the joint type is revolute or continuous, the smaller difference
+        (+/- 2*pi) is calculated.
+
+        Parameters
+        ----------
+        other : :class:`Configuration`
+            The configuration to compare to.
+
+        Yields
+        ------
+        float
+            The next difference to the `Configuration`'s values.
+
+        Raises
+        ------
+        ValueError
+            If the configurations are not comparable.
+
+        Examples
+        --------
+        >>> c1 = Configuration.from_revolute_values([1, 0, 3])
+        >>> c2 = Configuration.from_revolute_values([1, 2 * pi, 4])
+        >>> list(c1.differences_generator(c2))
+        [0, 0.0, -1]
+        """
+        if len(self.joint_names) and len(other.joint_names):
+            d1 = dict(zip(self.joint_names, self.values))
+            d2 = dict(zip(other.joint_names, other.values))
+            types = dict(zip(self.joint_names, self.types))
+            for k, v in d1.items():
+                if k not in d2:
+                    raise ValueError("Configurations have different joint names.")
+                diff = v - d2[k]
+                if types[k] in [Joint.REVOLUTE, Joint.CONTINUOUS]:
+                    sign = -1 if diff > 0 else 1
+                    while abs(diff) > abs(diff + sign * 2 * pi):
+                        diff = diff + sign * 2 * pi
+                yield diff
+        else:
+            if len(self.values) != len(other.values):
+                raise ValueError("Can't compare configurations with different lengths of values.")
+            for i, (v1, v2) in enumerate(zip(self.values, other.values)):
+                diff = v1 - v2
+                if self.types[i] in [Joint.REVOLUTE, Joint.CONTINUOUS]:
+                    sign = -1 if diff > 0 else 1
+                    while abs(diff) > abs(diff + sign * 2 * pi):
+                        diff = diff + sign * 2 * pi
+                yield diff
+
+    def max_difference(self, other):
+        """Returns the maximum difference to another `Configuration`'s values.
+
+        Parameters
+        ----------
+        other : :class:`Configuration`
+            The configuration to compare to.
+
+        Returns
+        ------
+        float
+            The maximum absolute difference
+
+        Examples
+        --------
+        >>> c1 = Configuration.from_revolute_values([1, 0, 3])
+        >>> c2 = Configuration.from_revolute_values([1, 2 * pi, 4])
+        >>> c1.max_difference(c2)
+        1
+        """
+        return max([abs(v) for v in self.differences_generator(other)])
+
+    def close_to(self, other, tol=1e-3):
+        """Returns `True` if the other `Configuration`'s values are within a certain range.
+
+        Parameters
+        ----------
+        other : :class:`Configuration`
+            The configuration to compare to.
+        tol : float
+            The tolerance under which we consider 2 floats the same. Defaults to 1e-3.
+
+        Returns
+        -------
+        bool
+            `True` if the other `Configuration`'s values are within a certain
+            tolerance, `False` otherwise.
+
+        Examples
+        --------
+        >>> c1 = Configuration.from_revolute_values([1, 0, 3])
+        >>> c2 = Configuration.from_revolute_values([1, 2 * pi, 3])
+        >>> c1.close_to(c2)
+        True
+        """
+        for diff in self.differences_generator(other):
+            if abs(diff) > tol:
+                return False
+        return True
+
 
 if __name__ == "__main__":
-    c1 = Configuration.from_revolute_values([1, 2, 3], joint_names=['1', '2', '3'])
-    c2 = Configuration.from_revolute_values([1, 2], joint_names=['1', '2'])
-    c1 = Configuration.from_revolute_values([1.0001, 2.0002 - 2 * 3.4, 3.0009], joint_names=['1', '2', '3'])
-    c2 = Configuration.from_revolute_values([1, 2], joint_names=['1', '2'])
+    import math  # noqa F401
+    import doctest
+    doctest.testmod(globs=globals())
