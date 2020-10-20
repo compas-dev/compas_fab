7731fdad1bc273dc3a956cfd1f9ce0108e1a3745
Gonzalo Casas <casas@arch.ethz.ch>
2020-10-20 18:18:47 +0200

    Test if it's the env variable failing

--- tests/ipy_test_runner.py
+++ tests/ipy_test_runner.py
@@ -1,12 +1,18 @@
 from __future__ import print_function
 
 import os
+import sys
 
 import pytest
 
 HERE = os.path.dirname(__file__)
+SRC = os.path.abspath(os.path.join(HERE, '..', 'src'))
 
 if __name__ == '__main__':
+    # Configure path to source
+    if SRC not in sys.path:
+        sys.path.append(SRC)
+
     # Fake Rhino modules
     pytest.load_fake_module('Rhino')
     pytest.load_fake_module('Rhino.Geometry', fake_types=['RTree', 'Sphere', 'Point3d'])
