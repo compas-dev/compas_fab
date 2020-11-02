f820f600b6a16ad3715415115bd61d95f39067fa
Gonzalo Casas <casas@arch.ethz.ch>
2020-11-03 00:34:13 +0100

    Fix doc warnings

--- src/compas_fab/blender/__init__.py
+++ src/compas_fab/blender/__init__.py
@@ -13,14 +13,8 @@ datastructures and models, in a way that maintains the data separated from the
 specific CAD interfaces, while providing a way to leverage native performance
 of the CAD environment.
 
-This package provides a robot artists implementation that is optimized to use
-in Blender.
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    RobotModelArtist
+This package only provides a deprecated alias to the
+:class:`~compas_blender.artists.RobotModelArtist` implemented in COMPAS.
 
 """
 try:
