8a8ca6a6a876b39d2c63ab6f7bb96b1bcd5ee875
Romana Rust <rust@arch.ethz.ch>
2020-11-25 14:16:29 +0100

    Update compile.py

--- src/compas_fab/ghpython/components/compile.py
+++ src/compas_fab/ghpython/components/compile.py
@@ -1,9 +1,7 @@
-from shutil import copyfile
-import os
 import clr
 
-from .assembly_info import assembly_name
-from .assembly_info import assembly_version
+assembly_name = "COMPAS FAB"
+assembly_version = "0.0.1"
 
 filename = "%s_%s.ghpy" % (assembly_name, assembly_version)
 clr.CompileModules(filename,
@@ -11,8 +9,19 @@ clr.CompileModules(filename,
                    "ros_connect.py",
                    "ros_robot.py",
                    "planning_scene.py",
-                   "collision_mesh.py")
+                   "collision_mesh.py",
+                   "attached_collision_mesh.py",
+                   "inverse_kinematics.py",
+                   "plan_cartesian_motion.py",
+                   "constraints_from_plane.py",
+                   "plan_motion.py",
+                   "visualize_robot.py",
+                   "visualize_trajectory.py",
+                   )
 
+
+from shutil import copyfile
+import os
 src = os.path.join(os.path.dirname(__file__), filename)
 dst = os.path.join(r'C:\Users\rustr\AppData\Roaming\Grasshopper\Libraries', filename)
 copyfile(src, dst)
