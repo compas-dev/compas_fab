bf3b3237e8307c2f8672b939b2adf7bee420a84f
Romana Rust <rust@arch.ethz.ch>
2021-01-08 17:16:03 +0100

    adding assembly into to init

--- src/compas_fab/ghpython/components/__init__.py
+++ src/compas_fab/ghpython/components/__init__.py
@@ -1,2 +1,7 @@
+assembly_name = "COMPAS FAB"
+assembly_version = "0.15.0"
+filename = "%s_%s.ghpy" % (assembly_name, assembly_version)
+
+
 def create_id(component, name):
     return '%s_%s' % (name, component.InstanceGuid)
