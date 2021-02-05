266ca9595b0ce84390f865b47b5bb15d2389e447
Romana Rust <rust@arch.ethz.ch>
2021-02-05 10:05:58 +0100

    Update icons.py

--- src/compas_fab/ghpython/components/icons.py
+++ src/compas_fab/ghpython/components/icons.py
@@ -12,5 +12,19 @@ def bitmap_from_image_path(image_path):
     return System.Drawing.Bitmap(System.IO.MemoryStream(System.Convert.FromBase64String(img_string)))
 
 
-ros_connect_icon = bitmap_from_image_path(os.path.join(icon_path, "ros_connect.png"))
-default_icon = bitmap_from_image_path(os.path.join(icon_path, "compas_icon_white.png"))
+attached_collision_mesh_icon = bitmap_from_image_path(os.path.join(icon_path, 'attached_collision_mesh.png'))
+collision_mesh_icon = bitmap_from_image_path(os.path.join(icon_path, 'collision_mesh.png'))
+contraints_from_plane_icon = bitmap_from_image_path(os.path.join(icon_path, 'contraints_from_plane.png'))
+inverse_kinematics_icon = bitmap_from_image_path(os.path.join(icon_path, 'inverse_kinematics.png'))
+planning_scene_icon = bitmap_from_image_path(os.path.join(icon_path, 'planning_scene.png'))
+plan_cartesian_motion_icon = bitmap_from_image_path(os.path.join(icon_path, 'plan_cartesian_motion.png'))
+plan_motion_icon = bitmap_from_image_path(os.path.join(icon_path, 'plan_motion.png'))
+robot_visualize_icon = bitmap_from_image_path(os.path.join(icon_path, 'robot_visualize.png'))
+ros_connect_icon = bitmap_from_image_path(os.path.join(icon_path, 'ros_connect.png'))
+ros_robot_icon = bitmap_from_image_path(os.path.join(icon_path, 'ros_robot.png'))
+trajectory_visualize_icon = bitmap_from_image_path(os.path.join(icon_path, 'trajectory_visualize.png'))
+
+
+# for img in os.listdir(icon_path):
+#    basename = os.path.splitext(img)[0]
+#    print("%s_icon = bitmap_from_image_path(os.path.join(icon_path, '%s.png'))" % (basename, basename))
