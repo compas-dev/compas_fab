4273585aca7bfe313542fa9c3f081063d2449133
Romana Rust <rust@arch.ethz.ch>
2021-02-11 19:15:28 +0100

    changing author email

--- src/compas_fab/ghpython/components/assembly_info.py
+++ src/compas_fab/ghpython/components/assembly_info.py
@@ -3,7 +3,7 @@ import System
 
 from compas_fab.ghpython.components import assembly_name
 from compas_fab.ghpython.components import assembly_version
-
+from compas_fab.__version__ import __author_email__
 
 class AssemblyInfo(GhPython.Assemblies.PythonAssemblyInfo):
     def get_AssemblyName(self):
@@ -16,7 +16,7 @@ class AssemblyInfo(GhPython.Assemblies.PythonAssemblyInfo):
         return assembly_version
 
     def get_AuthorName(self):
-        return "rust@arch.ethz.ch"
+        return __author_email__
 
     def get_Id(self):
         return System.Guid("237883f1-9678-4abe-9e20-2ceed9e6c611")
