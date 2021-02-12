c9c7a37e50ebaa7f01977826d10d4529e9c962e2
Romana Rust <rust@arch.ethz.ch>
2021-02-12 08:51:52 +0100

    change assembly nameing and add coerce_frame

--- src/compas_fab/ghpython/components/__init__.py
+++ src/compas_fab/ghpython/components/__init__.py
@@ -1,7 +1,19 @@
+import compas_fab
+
 assembly_name = 'COMPAS FAB'
-assembly_version = '0.15.0'
+assembly_version = compas_fab.__version__.split('-')[0]
 filename = '{}_{}.ghpy'.format(assembly_name, assembly_version)
 
 
 def create_id(component, name):
     return '{}_{}'.format(name, component.InstanceGuid)
+
+def coerce_frame(plane):
+    import Rhino
+    from compas.geometry import Frame
+    if isinstance(plane, Rhino.Geometry.Plane):
+        return Frame(plane.Origin, plane.XAxis, plane.YAxis)
+    elif isinstance(plane, Frame):
+        return plane
+    else:
+        return Frame(*plane)
