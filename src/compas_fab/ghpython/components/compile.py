6e68dbd7440fa68a58606bb9100495583bc79980
Gonzalo Casas <casas@arch.ethz.ch>
2021-02-24 00:37:36 +0100

    lint

--- src/compas_fab/ghpython/components/compile.py
+++ src/compas_fab/ghpython/components/compile.py
@@ -5,7 +5,7 @@ from compas_fab.ghpython.components import get_ghpy_filename
 
 
 def compile(version):
-    pythonfiles = glob.glob("*.py")
+    pythonfiles = glob.glob('*.py')
     not_include = ['__init__.py', 'icons.py', 'install.py', os.path.basename(__file__)]
     pythonfiles = list(set(pythonfiles) - set(not_include))
     filename = get_ghpy_filename(version)
