ee25475fcdf0bf3b5b5cd0a6a27b20d79edb3c82
Gonzalo Casas <casas@arch.ethz.ch>
2021-03-27 11:08:52 +0100

    Update install instructions to match COMPAS

--- docs/conf.py
+++ docs/conf.py
@@ -92,6 +92,7 @@ html_static_path = ['_static']
 html_last_updated_fmt = '%b %d, %Y'
 html_copy_source = False
 html_show_sourcelink = False
+html_permalinks = False
 html_add_permalinks = ''
 html_experimental_html5_writer = True
 html_compact_lists = True
