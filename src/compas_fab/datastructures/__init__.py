ccbca4b7d5b8f7f693374d8db0b076254eab3efe
beverly <lytle@arch.ethz.ch>
2021-04-12 09:30:36 +0200

    Add module level docstring

--- src/compas_fab/datastructures/__init__.py
+++ src/compas_fab/datastructures/__init__.py
@@ -1,3 +1,25 @@
+"""
+********************************************************************************
+compas_fab.datastructures
+********************************************************************************
+
+.. currentmodule:: compas_fab.datastructures
+
+Plan
+-----
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    Action
+    DependencyIdException
+    IntegerIdGenerator
+    Plan
+    PlannedAction
+
+"""
+
 from .plan import (
     Action,
     DependencyIdException,
