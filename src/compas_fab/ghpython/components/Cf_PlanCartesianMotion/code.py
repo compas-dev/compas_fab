6f5c66fb0e02412e398d439fd74914024fb8c43e
Gonzalo Casas <casas@arch.ethz.ch>
2021-05-14 19:29:16 +0200

    Bump version to 0.18.2

--- src/compas_fab/ghpython/components/Cf_PlanCartesianMotion/code.py
+++ src/compas_fab/ghpython/components/Cf_PlanCartesianMotion/code.py
@@ -1,7 +1,7 @@
 """
 Calculate a cartesian motion path (linear in tool space).
 
-COMPAS FAB v0.18.1
+COMPAS FAB v0.18.2
 """
 from ghpythonlib.componentbase import executingcomponent as component
 from scriptcontext import sticky as st
--- src/compas_fab/ghpython/components/Cf_RosRobot/code.py
+++ src/compas_fab/ghpython/components/Cf_RosRobot/code.py
@@ -1,7 +1,7 @@
 """
 Load robot directly from ROS.
 
-COMPAS FAB v0.18.1
+COMPAS FAB v0.18.2
 """
 from compas_ghpython.artists import RobotModelArtist
 from ghpythonlib.componentbase import executingcomponent as component
