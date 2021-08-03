6a005c886d81db05d1580ab7adc0bea595e53f11
Romana Rust <rust@arch.ethz.ch>
2021-08-03 19:57:00 +0200

    Update __init__.py

--- src/compas_fab/backends/ik_solver/__init__.py
+++ src/compas_fab/backends/ik_solver/__init__.py
@@ -2,3 +2,6 @@ from .utils import *
 from .ik_spherical_wrist import *
 from .spherical_wrist_robots import *
 from .ik_solver import *
+from .offset_wrist_kinematics import UR3Kinematics
+from .offset_wrist_kinematics import UR5Kinematics
+from .offset_wrist_kinematics import UR10Kinematics
