3730280d922bb36bf6d2ca5b9efc28215802104b
Romana Rust <rust@arch.ethz.ch>
2021-11-24 15:38:30 +0100

    adding URe

--- src/compas_fab/backends/kinematics/offset_wrist_kinematics.py
+++ src/compas_fab/backends/kinematics/offset_wrist_kinematics.py
@@ -1,5 +1,5 @@
-from compas_fab.backends.kinematics.offset_wrist import inverse_kinematics_offset_wrist
-from compas_fab.backends.kinematics.offset_wrist import forward_kinematics_offset_wrist
+from .offset_wrist import inverse_kinematics_offset_wrist
+from .offset_wrist import forward_kinematics_offset_wrist
 
 
 class OffsetWristKinematics(object):
@@ -30,6 +30,15 @@ UR5_PARAMS = {'d1':  0.089159,
               'd5':  0.09465,
               'd6':  0.0823}
 
+# https://www.universal-robots.com/articles/ur/application-installation/dh-parameters-for-calculations-of-kinematics-and-dynamics/
+UR5e_PARAMS = {'d1':  0.1625,
+               'a2': -0.425,
+               'a3': -0.3922,
+               'd4':  0.1333,
+               'd5':  0.0997,
+               'd6':  0.0996}
+
+
 UR3_PARAMS = {'d1':  0.1519,
               'a2': -0.24365,
               'a3': -0.21325,
@@ -54,6 +63,15 @@ class UR5(OffsetWristKinematics):
     def __init__(self):
         params = [UR5_PARAMS[k] for k in ['d1', 'a2', 'a3', 'd4', 'd5', 'd6']]
         super(UR5, self).__init__(params)
+
+
+class UR5e(OffsetWristKinematics):
+    """
+    """
+
+    def __init__(self):
+        params = [UR5e_PARAMS[k] for k in ['d1', 'a2', 'a3', 'd4', 'd5', 'd6']]
+        super(UR5e, self).__init__(params)
 
 
 class UR10(OffsetWristKinematics):
@@ -81,6 +99,12 @@ if __name__ == "__main__":
     assert(allclose(sol[0], q))
 
     kin = UR10()
+    q = [0.2, 0.5, 1.4, 1.3, 2.6, 2.3]
+    frame = kin.forward(q)
+    sol = kin.inverse(frame)
+    assert(allclose(sol[0], q))
+
+    kin = UR5e()
     q = [0.2, 0.5, 1.4, 1.3, 2.6, 2.3]
     frame = kin.forward(q)
     sol = kin.inverse(frame)
