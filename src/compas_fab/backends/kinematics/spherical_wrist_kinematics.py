fe777c5bafe37284f4bc7085cb50570c5be642ed
Romana Rust <rust@arch.ethz.ch>
2021-11-26 14:44:54 +0100

    Update spherical_wrist_kinematics.py

--- src/compas_fab/backends/kinematics/spherical_wrist_kinematics.py
+++ src/compas_fab/backends/kinematics/spherical_wrist_kinematics.py
@@ -1,8 +1,8 @@
 
 import math
 from compas.geometry import Point
-from compas_fab.backends.kinematics.spherical_wrist import forward_kinematics_spherical_wrist
-from compas_fab.backends.kinematics.spherical_wrist import inverse_kinematics_spherical_wrist
+from .spherical_wrist import forward_kinematics_spherical_wrist
+from .spherical_wrist import inverse_kinematics_spherical_wrist
 
 
 class SphericalWristKinematics(object):
@@ -28,7 +28,7 @@ class SphericalWristKinematics(object):
         return joint_values
 
 
-class Staubli_TX2_60L(SphericalWristKinematics):
+class Staubli_TX260LKinematics(SphericalWristKinematics):
     """
     """
 
@@ -37,7 +37,7 @@ class Staubli_TX2_60L(SphericalWristKinematics):
                   Point(0.000, 0.020, 0.775),
                   Point(0.450, 0.020, 0.775),
                   Point(0.520, 0.020, 0.775)]
-        super(Staubli_TX2_60L, self).__init__(points)
+        super(Staubli_TX260LKinematics, self).__init__(points)
 
     def _pre_process(self, joint_values):
         q1, q2, q3, q4, q5, q6 = joint_values
@@ -56,7 +56,7 @@ class Staubli_TX2_60L(SphericalWristKinematics):
             yield [q1, q2, q3, q4, q5, q6]
 
 
-class ABB_IRB_4600_40_255(SphericalWristKinematics):
+class ABB_IRB4600_40_255Kinematics(SphericalWristKinematics):
     """
     """
 
@@ -65,7 +65,7 @@ class ABB_IRB_4600_40_255(SphericalWristKinematics):
                   Point(0.175, 0.000, 1.590),
                   Point(1.446, 0.000, 1.765),
                   Point(1.581, 0.000, 1.765)]
-        super(ABB_IRB_4600_40_255, self).__init__(points)
+        super(ABB_IRB4600_40_255Kinematics, self).__init__(points)
 
     def _pre_process(self, joint_values):
         q1, q2, q3, q4, q5, q6 = joint_values
@@ -84,19 +84,3 @@ class ABB_IRB_4600_40_255(SphericalWristKinematics):
             q4 = q4 * -1
             q6 = q6 * -1 + math.pi / 2
             yield [q1, q2, q3, q4, q5, q6]
-
-
-if __name__ == "__main__":
-    from compas.geometry import allclose
-
-    kin = ABB_IRB_4600_40_255()
-    q = [0.2, 0.5, 1.4, 1.3, 2.6, 2.3]
-    frame = kin.forward(q)
-    sol = kin.inverse(frame)
-    assert(allclose(sol[0], q))
-
-    kin = Staubli_TX2_60L()
-    q = [0.2, 0.5, 1.4, 1.3, 2.6, 2.3]
-    frame = kin.forward(q)
-    sol = kin.inverse(frame)
-    assert(allclose(sol[0], q))
