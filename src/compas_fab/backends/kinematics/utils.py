7e14f0c2287f175f6fec4284d316de1205085364
Romana Rust <rust@arch.ethz.ch>
2021-11-26 14:59:21 +0100

    Update utils.py

--- src/compas_fab/backends/kinematics/utils.py
+++ src/compas_fab/backends/kinematics/utils.py
@@ -1,10 +1,14 @@
 import math
-from compas.geometry import argmin
 
-def get_smaller_angle(angle): # delete
+
+def get_smaller_angle(angle):  # delete
     angle1 = angle % (2 * math.pi)
     angle2 = angle1 - 2 * math.pi
     return angle1 if angle1 < math.fabs(angle2) else angle2
+
+
+def smallest_joint_angles(joint_angles):  # move to configuration ?
+    return [get_smaller_angle(j) for j in joint_angles]
 
 
 def fit_within_bounds(angle, lower, upper):
@@ -14,13 +18,3 @@ def fit_within_bounds(angle, lower, upper):
         angle -= 2 * math.pi
     assert(angle >= lower and angle <= upper)
     return angle
-
-
-def find_closest_configuration(configurations, configuration): # should move to configurations ?
-    diffs = []
-    configs = []
-    for c in configurations:
-        if c:
-            configs.append(c)
-            diffs.append(sum([abs(d) for d in configuration.iter_differences(c)]))
-    return argmin(diffs)
