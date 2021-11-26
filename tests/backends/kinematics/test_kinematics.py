cbb3d58216d0a1b50c01b1522d918069c991668c
Romana Rust <rust@arch.ethz.ch>
2021-11-26 14:44:43 +0100

    Update test_kinematics.py

--- tests/backends/kinematics/test_kinematics.py
+++ tests/backends/kinematics/test_kinematics.py
@@ -1,9 +1,24 @@
 from compas.geometry import allclose
 
 from compas_fab.backends.kinematics.offset_wrist_kinematics import UR5eKinematics
+from compas_fab.backends.kinematics.spherical_wrist_kinematics import Staubli_TX260LKinematics
+from compas_fab.backends.kinematics.spherical_wrist_kinematics import ABB_IRB4600_40_255Kinematics
+
 
 def test_kinematic_functions():
-    kin = UR5eKinematics()
+    kin = UR5eKinematics()  # one UR is enough, they are all the same
+    q = [0.2, 0.5, 1.4, 1.3, 2.6, 2.3]
+    frame = kin.forward(q)
+    sol = kin.inverse(frame)
+    assert(allclose(sol[0], q))
+
+    kin = Staubli_TX260LKinematics()
+    q = [0.2, 0.5, 1.4, 1.3, 2.6, 2.3]
+    frame = kin.forward(q)
+    sol = kin.inverse(frame)
+    assert(allclose(sol[0], q))
+
+    kin = ABB_IRB4600_40_255Kinematics()
     q = [0.2, 0.5, 1.4, 1.3, 2.6, 2.3]
     frame = kin.forward(q)
     sol = kin.inverse(frame)
