75e6bf7e9d7cdf1abbec5943a65c2d87650f7b17
Romana Rust <rust@arch.ethz.ch>
2021-12-07 17:56:20 +0100

    Update __init__.py

--- src/compas_fab/backends/kinematics/__init__.py
+++ src/compas_fab/backends/kinematics/__init__.py
@@ -1,2 +1,4 @@
 from .analytical_inverse_kinematics import AnalyticalInverseKinematics  # noqa: F401
+from .solvers import IK_SOLVERS  # noqa: F401
 from .analytical_plan_cartesian_motion import AnalyticalPlanCartesianMotion  # noqa: F401
+from .client import AnalyticalPyBulletClient  # noqa: F401
