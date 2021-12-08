fcd943175c605f04e06fd91236249e5832376a4d
Romana Rust <rust@arch.ethz.ch>
2021-12-08 08:32:48 +0100

    change to planner_backends

--- src/compas_fab/backends/kinematics/solvers/__init__.py
+++ src/compas_fab/backends/kinematics/solvers/__init__.py
@@ -2,13 +2,13 @@ from .spherical_wrist_kinematics import *  # noqa: F403
 from .offset_wrist_kinematics import *  # noqa: F403
 
 
-IK_SOLVERS = {
-    'ur3': UR3Kinematics().inverse,  # noqa: F405
-    'ur3e': UR3eKinematics().inverse,  # noqa: F405
-    'ur5': UR5Kinematics().inverse,  # noqa: F405
-    'ur5e': UR5eKinematics().inverse,  # noqa: F405
-    'ur10': UR10Kinematics().inverse,  # noqa: F405
-    'ur10e': UR10eKinematics().inverse,  # noqa: F405
-    'staubli_tx260l': Staubli_TX260LKinematics().inverse,  # noqa: F405
-    'abb_irb4600_40_255': ABB_IRB4600_40_255Kinematics().inverse,  # noqa: F405
+PLANNER_BACKENDS = {
+    'ur3': UR3Kinematics,  # noqa: F405
+    'ur3e': UR3eKinematics,  # noqa: F405
+    'ur5': UR5Kinematics,  # noqa: F405
+    'ur5e': UR5eKinematics,  # noqa: F405
+    'ur10': UR10Kinematics,  # noqa: F405
+    'ur10e': UR10eKinematics,  # noqa: F405
+    'staubli_tx260l': Staubli_TX260LKinematics,  # noqa: F405
+    'abb_irb4600_40_255': ABB_IRB4600_40_255Kinematics,  # noqa: F405
 }
