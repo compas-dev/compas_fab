8b4f65bf3fd89171a76e08c88804c225f99046f4
Chen Kasirer <kasirer@arch.ethz.ch>
2022-06-02 14:36:20 +0200

    added a ConstraintsFromConfiguration GH component

---
+++ src/compas_fab/ghpython/components/Cf_ConstraintsFromConfiguration/code.py
@@ -0,0 +1,24 @@
+"""
+Create a position and an orientation constraint from a plane calculated for the group's end-effector link.
+
+COMPAS FAB v0.25.0
+"""
+import math
+
+from ghpythonlib.componentbase import executingcomponent as component
+
+
+class ConstraintsFromTargetConfiguration(component):
+    def RunScript(self, robot, target_configuration, tolerance_above, tolerance_below, group_name):
+        if robot and target_configuration:
+            tolerance_above = tolerance_above or [math.radians(1)] * 6
+            tolerance_below = tolerance_below or [math.radians(1)] * 6
+
+            constraints = robot.constraints_from_configuration(
+                configuration=target_configuration,
+                tolerances_above=tolerance_above,
+                tolerances_below=tolerance_below,
+                group=group_name
+            )
+
+            return constraints
