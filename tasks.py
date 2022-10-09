f838a43b6de2f3185c20019ffef34c449367e4a6
Gonzalo Casas <casas@arch.ethz.ch>
2022-10-09 15:41:15 +0200

    switch to the latest invocations with flake8 + black as linters

--- tasks.py
+++ tasks.py
@@ -2,32 +2,16 @@ from __future__ import print_function
 
 import os
 
-from invoke import task
-from invoke import Collection
-
-from compas_invocations.console import chdir
 from compas_invocations import build
 from compas_invocations import docs
 from compas_invocations import style
 from compas_invocations import tests
-
-BASE_FOLDER = os.path.dirname(__file__)
-
-
-@task
-def lint(ctx):
-    """Check the consistency of coding style."""
-    print("Running flake8 python linter...")
-
-    with chdir(BASE_FOLDER):
-        ctx.run("flake8 src")
-
+from invoke import Collection
 
 ns = Collection(
     docs.help,
     style.check,
-    lint,  # until we switch to black, we keep this local flake8-based lint task
-    # style.lint,
+    style.lint,
     style.format,
     docs.docs,
     docs.linkcheck,
@@ -41,7 +25,7 @@ ns = Collection(
 )
 ns.configure(
     {
-        "base_folder": BASE_FOLDER,
+        "base_folder": os.path.dirname(__file__),
         "ghuser": {
             "source_dir": "src/compas_fab/ghpython/components",
             "target_dir": "src/compas_fab/ghpython/components/ghuser",
