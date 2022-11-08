c07c3d5b5c0329df94e0cd11a113ecb1568d7bb7
Gonzalo Casas <casas@arch.ethz.ch>
2022-11-09 00:49:35 +0100

    Add FK gh component

---
+++ src/compas_fab/ghpython/components/Cf_ForwardKinematics/code.py
@@ -0,0 +1,20 @@
+"""
+Calculate the robot's forward kinematic for a given configuration.
+
+COMPAS FAB v0.27.0
+"""
+from compas_rhino.conversions import RhinoPlane
+from ghpythonlib.componentbase import executingcomponent as component
+
+
+class ForwardKinematics(component):
+    def RunScript(self, robot, config, group, link, use_only_model):
+        frame = None
+        if robot and config:
+            options = {}
+            if link:
+                options["link"] = link
+            if use_only_model:
+                options["solver"] = "model"
+            frame = robot.forward_kinematics(config, group, options=options)
+        return frame
