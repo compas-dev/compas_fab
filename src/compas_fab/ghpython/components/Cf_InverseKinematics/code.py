daf6d279d5be722c1ccd422e44cee1c85e547e98
Gonzalo Casas <casas@arch.ethz.ch>
2024-01-11 03:13:39 +0100

    Updated rhino conversions

--- src/compas_fab/ghpython/components/Cf_InverseKinematics/code.py
+++ src/compas_fab/ghpython/components/Cf_InverseKinematics/code.py
@@ -3,7 +3,7 @@ Calculate the robot's inverse kinematic for a given plane.
 
 COMPAS FAB v0.28.0
 """
-from compas_rhino.conversions import RhinoPlane
+from compas_rhino.conversions import plane_to_compas_frame
 from ghpythonlib.componentbase import executingcomponent as component
 
 
@@ -11,6 +11,6 @@ class InverseKinematics(component):
     def RunScript(self, robot, plane, start_configuration, group):
         configuration = None
         if robot and robot.client and robot.client.is_connected and plane:
-            frame = RhinoPlane.from_geometry(plane).to_compas_frame()
+            frame = plane_to_compas_frame(plane)
             configuration = robot.inverse_kinematics(frame, start_configuration, group)
         return configuration
