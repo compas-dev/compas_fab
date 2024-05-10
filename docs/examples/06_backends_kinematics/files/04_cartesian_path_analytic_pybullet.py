9eb80fb4222fcaad6f84d49b4d9fe51bd66eca91
Victor LEUNG <yck011522@gmail.com>
2024-05-10 10:42:44 +0800

    changed 3 doc tests files

--- docs/examples/06_backends_kinematics/files/04_cartesian_path_analytic_pybullet.py
+++ docs/examples/06_backends_kinematics/files/04_cartesian_path_analytic_pybullet.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from compas.geometry import Frame
 
 from compas_fab.backends import AnalyticalPyBulletClient
+from compas_fab.robots import FrameWaypoints
 
 frames_WCF = [
     Frame((0.407, 0.073, 0.320), (0.922, 0.000, 0.388), (0.113, 0.956, -0.269)),
@@ -16,7 +17,8 @@ with AnalyticalPyBulletClient(connection_type="direct") as client:
 
     options = {"solver": "ur5", "check_collision": True}
     start_configuration = list(robot.iter_inverse_kinematics(frames_WCF[0], options=options))[-1]
-    trajectory = robot.plan_cartesian_motion(frames_WCF, start_configuration=start_configuration, options=options)
+    waypoints = FrameWaypoints(frames_WCF)
+    trajectory = robot.plan_cartesian_motion(waypoints, start_configuration=start_configuration, options=options)
     assert trajectory.fraction == 1.0
 
     j = [c.joint_values for c in trajectory.points]
