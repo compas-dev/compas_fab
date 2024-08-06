3c66faa52317448a923f159ce8bbf48020b785b5
Victor LEUNG <yck011522@gmail.com>
2024-08-06 14:53:24 +0800

    IK Examples with tools

--- docs/examples/05_backends_pybullet/files/02_inverse_kinematics_with_tools.py
+++ docs/examples/05_backends_pybullet/files/02_inverse_kinematics_with_tools.py
@@ -3,21 +3,17 @@ from compas_fab.backends import PyBulletClient
 from compas_fab.backends import PyBulletPlanner
 
 from compas_fab.robots import FrameTarget
-from compas_fab.robots import RobotCell
-from compas_fab.robots import RobotCellState
-from compas_fab.robots import RobotLibrary
+from compas_fab.robots import RobotCellLibrary
 
 with PyBulletClient() as client:
     planner = PyBulletPlanner(client)
-    robot = RobotLibrary.ur5()
-    planner.set_robot_cell(RobotCell(robot))
+    robot_cell, robot_cell_state = RobotCellLibrary.ur5_cone_tool()
+    planner.set_robot_cell(robot_cell)
 
     frame_WCF = Frame([0.3, 0.1, 0.5], [1, 0, 0], [0, 1, 0])
     target = FrameTarget(frame_WCF)
 
-    start_configuration = robot.zero_configuration()
-    robot_cell_state = RobotCellState.from_robot_configuration(robot, start_configuration)
-
+    # The robot_cell_state from the RobotCellLibrary contains a zero configuration for the robot
     joint_positions, joint_names = planner.inverse_kinematics(target, robot_cell_state)
 
     print("Inverse kinematics result: ", joint_positions, joint_names)
