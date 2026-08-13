0fd8b3bedf6b19470a984dd52d3bc850c18178b3
Arda ÖZCAN <76063457+ardozcnn@users.noreply.github.com>
2026-08-13 23:46:27 +0300

    Update 04_ik_allow_collision.py

--- docs/backends/ros/files/04_ik_allow_collision.py
+++ docs/backends/ros/files/04_ik_allow_collision.py
@@ -1,7 +1,15 @@
-from typing import Any, Dict
-from compas.geometry import Box, Frame
-from compas_fab.backends import InverseKinematicsError, MoveItPlanner, RosClient
-from compas_fab.robots import FrameTarget, RigidBody, TargetMode
+from typing import Any
+from typing import Dict
+
+from compas.geometry import Box
+from compas.geometry import Frame
+
+from compas_fab.backends import InverseKinematicsError
+from compas_fab.backends import MoveItPlanner
+from compas_fab.backends import RosClient
+from compas_fab.robots import FrameTarget
+from compas_fab.robots import RigidBody
+from compas_fab.robots import TargetMode
 
 with RosClient() as client:
     planner = MoveItPlanner(client)
@@ -37,3 +45,12 @@ with RosClient() as client:
     print("Inverse kinematics with collision check disabled:")
     options = {"check_collision": False}
     calculate(options)
+
+"""
+Output:
+>>> Target frame with Robot Mode: FrameTarget(Frame(point=Point(x=0.300, y=0.100, z=0.500), xaxis=Vector(x=1.000, y=0.000, z=0.000), yaxis=Vector(x=0.000, y=1.000, z=0.000)), ROBOT)
+>>> Inverse kinematics with collision check enabled: (by default)
+>>>     Failed to find a configuration:  No inverse kinematics solution found.
+>>> Inverse kinematics with collision check disabled:
+>>>     Found configuration:  Configuration((-0.031, -2.025, 2.161, 4.576, -4.712, -1.540), (0, 0, 0, 0, 0, 0), ('shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'))
+"""
